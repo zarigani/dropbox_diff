0a98cb5735ef18c955b7d3343f91c46039ceeb8a
zariganitosh <mail@example.com>
2012-10-08 06:09:53 +0900

    重複の排除、処理の効率化

--- dropbox_curl.sh
+++ dropbox_curl.sh
@@ -4,20 +4,46 @@ FILE_PATH=$1
 FILE_NAME="${FILE_PATH##*/}"
 REVISION_FILE_PATH="https://www.dropbox.com/revisions/$FILE_NAME"
 
-read -p 'email-adress: ' EMAIL_ADRESS
-read -s -p 'password: ' PASSWORD; echo
 
-curl -L -c cookie.txt -o output.html https://www.dropbox.com/login
-TOKEN=`cat output.html | grep -e '<input type="hidden" name="t" value=".*" />' | grep -o 'value=".*"' | grep -o '".*"' | grep -o '[^"].*[^"]'`
-curl -L -b cookie.txt -c login_cookie.txt -o output.html \
-     --data-urlencode "t=$TOKEN" \
-     --data-urlencode "login_email=$EMAIL_ADRESS" \
-     --data-urlencode "login_password=$PASSWORD" \
-     https://www.dropbox.com/login
 
-curl -L -w "%{url_effective}" -b login_cookie.txt -o output.html $REVISION_FILE_PATH
+
+dropbox_login() {
+	read -p 'email-adress: ' EMAIL_ADRESS
+	read -s -p 'password: ' PASSWORD; echo
+	
+	curl -L -c cookie.txt -o output.html https://www.dropbox.com/login
+	TOKEN=`cat output.html | grep -e '<input type="hidden" name="t" value=".*" />' | grep -o 'value=".*"' | grep -o '".*"' | grep -o '[^"].*[^"]'`
+	curl -L -b cookie.txt -c login_cookie.txt -o output.html \
+	     --data-urlencode "t=$TOKEN" \
+	     --data-urlencode "login_email=$EMAIL_ADRESS" \
+	     --data-urlencode "login_password=$PASSWORD" \
+	     https://www.dropbox.com/login
+}
+
+revision_files_page() {
+	curl -L -w "%{url_effective}" -b login_cookie.txt -o output.html $REVISION_FILE_PATH
+}
+
+extract_file_urls() {
+	cat output.html|grep -o '<a href="https://dl-web.dropbox.com/get/.*</a>'|grep -o '"https://.*"'|grep -o '[^"].*[^"]'
+}
+
+download_revision_file() {
+	curl -s -b login_cookie.txt ${URLS[$1]}
+}
+
+
+
+
+# リダイレクトした時だけログインし直す
+RES=`revision_files_page`
+if [ $RES != $REVISION_FILE_PATH ]; then
+	dropbox_login
+	revision_files_page
+fi
+
 echo
-URLS=(`cat output.html|grep -o '<a href="https://dl-web.dropbox.com/get/.*</a>'|grep -o '"https://.*"'|grep -o '[^"].*[^"]'`)
+URLS=(`extract_file_urls`)
 echo ${URLS[@]}
-CONTENTS=`curl -s -b login_cookie.txt ${URLS[1]}`
+CONTENTS=`download_revision_file 1`
 echo -en $CONTENTS|diff -u - $FILE_PATH
