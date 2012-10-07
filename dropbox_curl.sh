96e141d719fcd4a0bd75a6964dd5f2f008f8c766
zariganitosh <mail@example.com>
2012-10-07 14:29:42 +0900

    login Dropbox by curl.

---
+++ dropbox_curl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+EMAIL_ADRESS=$1
+PASSWORD=$2
+FILE_NAME=$3
+
+curl -L -c cookie.txt -o output.html https://www.dropbox.com/login
+TOKEN=`cat output.html | grep -e '<input type="hidden" name="t" value=".*" />' | grep -o 'value=".*"' | grep -o '".*"' | grep -o '[^"].*[^"]'`
+curl -L -b cookie.txt -c login_cookie.txt -o output.html \
+--data-urlencode "t=$TOKEN" \
+--data-urlencode "login_email=$EMAIL_ADRESS" \
+--data-urlencode "login_password=$PASSWORD" \
+https://www.dropbox.com/login
+curl -L -b login_cookie.txt -o output.html "https://www.dropbox.com/revisions/$FILE_NAME"
